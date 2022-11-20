95da902d5e144dcedf38d609489a8ad330be844d
Raziya Tabassum <108030074+Raziiy@users.noreply.github.com>
2022-11-20 15:19:50 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,31 @@
+class pattern
+
+{
+ 
+ public static void main(String args[])
+ 
+ {
+   
+ int row, column;
+ 
+  for(row=0; row<6; row++)
+  
+  {
+      
+for(column=1; column<row; column++)
+     
+ {
+      
+  System.out.print("*");
+     
+ }
+    
+  // print the stars in new line 
+     
+ System.out.println();
+    
+}
+  
+}
+
+}
